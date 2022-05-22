678257d8068d9b85d7ec11a4f4e659350e9f0a26
knokkelmann <knokkelmanns@gmail.com>
2022-05-22 18:27:47 +0200

    changed urllib.request to requests

--- sber.py
+++ sber.py
@@ -1,12 +1,23 @@
-import urllib.request
+import requests
 import openpyxl
+
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
+              "application/signed-exchange;v=b3;q=0.9",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 "
+                  "Safari/537.36",
+    "bx-ajax": "true"
+}
 
 
 # download a file with bank branches where you can buy money
 def get_data():
     try:
-        file = "ADD_DIRECT_LINK_TO_XLSX_FROM_BANK"
-        urllib.request.urlretrieve(file, "exchange.xlsx")
+        r = requests.get(
+            url='ADD_DIRECT_LINK_TO_XLSX_FROM_BANK',
+            headers=headers)
+        with open("exchange.xlsx", "wb") as file:
+            file.write(r.content)
 
         return 'XLSX успешно скачан!'
 
@@ -29,13 +40,17 @@ def parse_data(city):
                 if f'г.{city}'.lower().replace('ё', 'е') in address_list.lower().replace('ё', 'е'):
                     print(address_list)
 
+                # for x, y in ('ё', '-'), ('е', ' '):
+                # if f'г.{city}'.lower().replace(x, y) in address_list.lower().replace(x, y):
+                # print(address_list)
+
     except Exception as _ex:
         return 'Упс... что-то пошло не так. Попробуй снова'
 
 
 def main():
     city = input('Введите название города на русском: ')
-    # print(get_data())
+    print(get_data())
     print(parse_data(city))
 
 
